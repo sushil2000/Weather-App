87aa95f8f6c0bf251b1a962eab1b01fb28e1c497
Sushil Sharma <sushil.s.ss44@gmail.com>
2024-05-22 18:42:11 +1200

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Sushil Sharma on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
