9e632cbd83d34bcaed2290158f08fcc899e8c64b
sushil2000 <sushil.s.ss44@gmail.com>
2024-05-22 19:08:25 +1200

    Created Location Manager repo to see if the location is valid.

--- Weather App/Managers/LocationManager.swift
+++ Weather App/Managers/LocationManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    // public variables
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func requestLocation(){
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    //locations[CLLocations] is an array
+    func locationManager(_ manager: CLLocationManager, didUpdateLocation locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        <#code#>print("Error getting location", error)
+        isLoading = false
+    }
+}
